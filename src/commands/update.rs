3815b090c6c263536bd07ab836903262565ec54b
Nicolas Gonzalez <ndgfrs@gmail.com>
2025-06-13 15:08:06 -0400

    refactor: create update_server_jar and update_config_file

--- src/commands/update.rs
+++ src/commands/update.rs
@@ -36,6 +36,7 @@ impl crate::commands::Run for Update {
             None => versions.last().with_context(|| "no versions available")?,
         };
 
+        // The `eprintln` macro will panic if writing to stderr fails.
         let mut stderr = std::io::stderr().lock();
         #[rustfmt::skip]
         writeln!(stderr, "Updating to Minecraft version {}", version.as_str().yellow()).ok();
@@ -49,56 +50,30 @@ impl crate::commands::Run for Update {
             .with_context(|| "no builds available")?;
 
         if build.experimental() && !self.allow_experimental {
-            let hint = get_latest_stable_version(&versions, version)
-                .map(|v| format!("The latest stable version is '{}'", v.as_str()))
-                .unwrap_or_default();
-
             let message = format!(
                 "Selected version is experimental. Use {} or set a stable version explicitly",
                 "--allow-experimental".yellow()
             );
+
+            let hint = get_latest_stable_version(&versions, version)
+                .map(|v| format!("The latest stable version is '{}'", v.as_str()))
+                .ok(); // Ignore the error if we are unable to get the latest stable version.
 
             return Err(crate::Error::new(message, None).with_hint(hint).into());
         }
 
         if !self.allow_downgrade {
             // TODO: Check the current version and ensure the target version is above it.
+            // TODO: Use semver to implement Version comparison operators.
         }
 
-        writeln!(stderr, "Downloading server.jar file...").ok();
         let directory = self.cwd.to_path_buf();
-        let jars = directory.join("jars");
-        let paper_jar = jars.join(build.download_name());
+        writeln!(stderr, "Downloading server.jar file...").ok();
+        update_server_jar(&directory, &build, self.timeout)?;
 
-        if !paper_jar.exists() {
-            std::fs::create_dir_all(&jars).with_context(|| "Failed to create 'jars' directory")?;
-            let data = build
-                .download(std::time::Duration::from_secs(self.timeout))
-                .with_context(|| "failed to download new server")?;
-
-            std::fs::write(&paper_jar, &data).with_context(|| "failed to save new server")?;
-        }
-
-        let server_directory = directory.join("server");
-        debug_assert!(server_directory.exists());
-        let server_jar = server_directory.join("server.jar");
-
-        if let Err(err) = std::fs::remove_file(&server_jar) {
-            match err.kind() {
-                std::io::ErrorKind::NotFound => (),
-                std::io::ErrorKind::IsADirectory => std::fs::remove_dir_all(&server_jar)?,
-                _ => return Err(err.into()),
-            }
-        }
-
-        symlink::symlink_file(&paper_jar, &server_jar)
-            .with_context(|| "failed to link new server.jar")?;
-
-        // Update configuration file to use new version.
-        let config_file = directory.join("Axiom.toml");
-        let mut doc = std::fs::read_to_string(&config_file)?.parse::<toml_edit::DocumentMut>()?;
-        doc["server"]["version"] = toml_edit::value(version.as_str());
-        std::fs::write(&config_file, doc.to_string())?;
+        // Update configuration file to reflect the new version.
+        update_config_file(crate::config::Config::path(directory), version)
+            .with_context(|| "failed to update version in Axiom.toml")?;
 
         #[rustfmt::skip]
         writeln!(stderr, "Server updated to Minecraft version {}", version.as_str().yellow()).ok();
@@ -110,7 +85,7 @@ impl crate::commands::Run for Update {
 fn get_latest_stable_version(
     supported_versions: &[paper::Version],
     selected: &paper::Version,
-) -> anyhow::Result<paper::Version> {
+) -> Result<paper::Version, anyhow::Error> {
     let mut older_versions: Vec<&paper::Version> = supported_versions
         .iter()
         .take_while(|&v| v.as_str() != selected.as_str())
@@ -129,4 +104,55 @@ fn get_latest_stable_version(
     }
 
     Err(anyhow::anyhow!("failed to find the latest stable version"))
+}
+
+fn update_server_jar(
+    base_directory: impl AsRef<std::path::Path>,
+    build: &paper::Build,
+    timeout: u64,
+) -> Result<(), anyhow::Error> {
+    let jars = base_directory.as_ref().join("jars");
+    let paper_jar = jars.join(build.download_name());
+
+    if !paper_jar.exists() {
+        std::fs::create_dir_all(&jars).with_context(|| "Failed to create 'jars' directory")?;
+        let data = build
+            .download(std::time::Duration::from_secs(timeout))
+            .with_context(|| "failed to download new server")?;
+
+        std::fs::write(&paper_jar, &data).with_context(|| "failed to save new server")?;
+    }
+
+    let server_directory = base_directory.as_ref().join("server");
+
+    // TODO: I don't think this should be an assertion since the user can call this subcommand
+    // themselves. Perhaps suggest they run the build command first? should I just create the
+    // directory for them? Why are you updating the server if you don't have a server yet? Is this
+    // just for the convenience of not having to open the configuration file? Is that okay?
+    debug_assert!(server_directory.exists());
+    let server_jar = server_directory.join("server.jar");
+
+    if let Err(err) = std::fs::remove_file(&server_jar) {
+        match err.kind() {
+            std::io::ErrorKind::NotFound => (),
+            std::io::ErrorKind::IsADirectory => std::fs::remove_dir_all(&server_jar)?,
+            _ => return Err(err.into()),
+        }
+    }
+
+    symlink::symlink_file(&paper_jar, &server_jar)
+        .with_context(|| "failed to link new server.jar")?;
+
+    Ok(())
+}
+
+fn update_config_file(
+    config_file: impl AsRef<std::path::Path>,
+    version: &paper::Version,
+) -> Result<(), anyhow::Error> {
+    let mut doc = std::fs::read_to_string(&config_file)?.parse::<toml_edit::DocumentMut>()?;
+    doc["server"]["version"] = toml_edit::value(version.as_str());
+    std::fs::write(&config_file, doc.to_string())?;
+
+    Ok(())
 }
